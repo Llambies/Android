fb1b793a7c8e9649431d7c42448252f94eb82356
Adrian <admallla@epsg.upv.es>
2021-10-18 04:32:15 +0200

    FuncionalidadBaseDatos

---
+++ src/sHumo/app/src/main/java/com/example/admallla/shumo/Medida.java
@@ -0,0 +1,45 @@
+package com.example.admallla.shumo;
+
+/**
+ *
+ * AUTOR: Adrian Maldonado Llambies
+ * FECHA: 17/10/2021
+ * DESCRIPCION: Clase medida
+ *
+ */
+public class Medida {
+
+    String fecha;
+    int valor;
+    String lat;
+    String lon;
+
+    /**
+     *
+     * Constructor
+     *
+     * @param fecha
+     * @param valor
+     * @param lat
+     * @param lon
+     */
+    public Medida(String fecha, int valor, String lat, String lon) {
+        this.fecha = fecha;
+        this.valor = valor;
+        this.lat = lat;
+        this.lon = lon;
+
+    }
+
+    /**
+     *
+     * String formateado en JSON
+     *
+     * @return
+     */
+    @Override
+    public String toString() {
+        return "{\"fecha\":\"" + fecha + "\" , \"valor\": \" " + valor + "\", \"lat\": \""+ lat +"\", \"lon\": \""+ lon +"\"}";
+
+    }
+}
